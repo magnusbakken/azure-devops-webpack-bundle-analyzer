44501f570a504cd4f223db31b0f3ad29cdcd141c
Magnus Grindal Bakken <magnusbakken@gmail.com>
2018-11-17 19:56:22 +0100

    Added webpack-bundle-analyzer.

--- tasks/generate-wba-report-task/index.ts
+++ tasks/generate-wba-report-task/index.ts
@@ -1,11 +1,27 @@
-import tl = require('azure-pipelines-task-lib/task')
+import process = require('process');
+import child_process = require('child_process');
+import tl = require('azure-pipelines-task-lib/task');
+
+const isWindows = process.platform === "win32";
+
+function execute(statsJsonPath: string): void {
+    const args = ['node_modules\\.bin\\webpack-bundle-analyzer', statsJsonPath, '--mode=static', '--report=report.html', '--no-open'];
+    if (isWindows) {
+        child_process.execFileSync('cmd.exe', ['/c', ...args]);
+    } else {
+        const [file, ...otherArgs] = args;
+        child_process.spawnSync(file, otherArgs);
+    }
+}
 
 async function run(): Promise<void> {
     try {
         const statsJsonPath: string = tl.getPathInput('statsJsonPath', true, true);
-        console.log('stats.json', statsJsonPath);
-    }
-    catch (err) {
+        console.log('Starting...');
+        execute(statsJsonPath);
+        console.log('Done');
+    } catch (err) {
+        console.log(err);
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
